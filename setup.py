807199e32aad0fbb1c50124be08bc3e3be886bfe
Troublem1 <immanuelsiawdarko@gmail.com>
2020-09-13 00:47:13 +0000

    fixed bug in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='MultiLabel_Encoder',
       version='0.1',
       author="Emmanuel Siaw Darko",
-      author_email="immanuelsiawdarko@gmail.com"
+      author_email="immanuelsiawdarko@gmail.com",
       description='An Extended Transformer version of LabelEncoder that preprocesses more than one column in a dataset.',
-      url = 'https://github.com/Troublem1/MLE'
+      url = 'https://github.com/Troublem1/MLE',
       packages =['MLE_Trans'],
       zip_safe = False)
